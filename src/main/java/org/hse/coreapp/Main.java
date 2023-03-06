2371be6fd9c0220d6f2a7603cccbb8e1eb29978e
Boris Mikhaylov <boris.n.mikhaylov@gmail.com>
2023-03-06 06:44:33 -0800

    mvp

--- src/main/java/org/hse/coreapp/Main.java
+++ src/main/java/org/hse/coreapp/Main.java
@@ -1,4 +1,13 @@
 package org.hse.coreapp;
 
+import com.itextpdf.text.DocumentException;
+import org.hse.mainbuilder.MainBuilder;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException, DocumentException {
+        MainBuilder runner = new MainBuilder("/Users/borismikhaylov/Downloads/sampleDiploma.xlsx");
+        runner.createPdfs();
+    }
 }
--- src/main/java/org/hse/mainbuilder/MainBuilder.java
+++ src/main/java/org/hse/mainbuilder/MainBuilder.java
@@ -1,39 +1,57 @@
 package org.hse.mainbuilder;
 
-import org.apache.poi.ss.usermodel.DataFormatter;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainBuilder {
 
-    public void parseSheets(){
-        for (Row row: sheet){
+    private void parseSheets() {
+        for (Row row : sheet) {
             String lastName = row.getCell(0).toString();
             String firstName = row.getCell(1).toString();
-            int place = (int)row.getCell(2).getNumericCellValue();
+            int place = (int) row.getCell(2).getNumericCellValue();
             participants.add(new Person(firstName, lastName, place));
         }
+    }
+
+    public void createPdfs() throws IOException, DocumentException {
+        for (Person person : participants) {
+            createDiploma(person);
+        }
+    }
+
+    private void createDiploma(Person person) throws IOException, DocumentException {
+        Document document = new Document();
+        FileOutputStream outputStream = new FileOutputStream(new File(""));
+        PdfWriter.getInstance(document, outputStream);
+        document.open();
+        document.add(new Paragraph("Congrats " + person.firstName + " " + person.lastName + "on " + person.position + " place!"));
+        document.close();
+        outputStream.close();
     }
 
     public MainBuilder(String sheetLocation) throws IOException {
         FileInputStream file = new FileInputStream(new File(sheetLocation));
         Workbook workbook = new XSSFWorkbook(file);
         sheet = workbook.getSheetAt(0);
+        parseSheets();
     }
 
     List<Person> participants = new ArrayList<>();
     Sheet sheet;
 }
 
-class Person{
+class Person {
     String firstName;
     String lastName;
     int position;
