00563f4a1309d0bad44cbe96737a09e89691ec41
Boris Mikhaylov <boris.n.mikhaylov@gmail.com>
2023-06-07 03:15:14 -0700

    mvp

---
+++ src/main/java/org/hse/mainbuilder/Person.java
@@ -0,0 +1,14 @@
+package org.hse.mainbuilder;
+
+public class Person {
+    String firstName;
+    String lastName;
+    int position;
+
+    public Person(String firstName, String lastName, int position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+    }
+}
+
